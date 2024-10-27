807a4fed3ab7065b9870f2ca0ae22fe87d15ab5b
dylanhill7 <mce8ep@virginia.edu>
2024-10-27 15:57:04 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Hello World"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
